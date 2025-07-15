3675adf80b9a16f93c3fb639a67a7581aee2a7a6
LucasDias47 <lucasdias9354@gmail.com>
2025-07-15 12:52:32 -0300

    Crie a classe ClienteMapper e fiz os ajustes necessarios

--- src/main/java/com/example/concessionaria/service/ClienteService.java
+++ src/main/java/com/example/concessionaria/service/ClienteService.java
@@ -2,14 +2,19 @@ package com.example.concessionaria.service;
 
 import java.util.List;
 
+
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.example.concessionaria.dto.cliente.ClienteRecordDto;
+import com.example.concessionaria.dto.cliente.ClienteResponseDto;
+import com.example.concessionaria.mapper.ClienteMapper;
 import com.example.concessionaria.model.ClienteModel;
 import com.example.concessionaria.repository.ClienteRepository;
+
 
 @Service
 public class ClienteService {
@@ -27,15 +32,21 @@ public class ClienteService {
 		return clienteRepository.findById(id);
 	}
 
-	public ClienteModel criarCliente(ClienteRecordDto dto) {
+	public ClienteResponseDto criarCliente(ClienteRecordDto dto) {
 		ClienteModel novo = new ClienteModel();
 		novo.setNome(dto.nome());
 		novo.setCpf(dto.cpf());
-		return clienteRepository.save(novo);
+		return ClienteMapper.toResponseDto(clienteRepository.save(novo));
 	}
 	
-	public List<ClienteModel> listarClientes(){
-		return clienteRepository.findAll();
+	public ClienteModel salvarModel(ClienteModel model) {
+		return clienteRepository.save(model);
+	}
+	
+	public List<ClienteRecordDto> listarClientes(){
+		return clienteRepository.findAll().stream()
+			.map(ClienteMapper::toDto)
+			.collect(Collectors.toList());
 	}
 	
 	public void deletarCliente(UUID id) {
